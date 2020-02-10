20c06d60732fe08e8aaa940a402c6c5f02886303
Andres <andresgarcia7960@gmail.com>
2020-02-10 16:38:42 -0600

    Autorizacion

--- Controllers/RetencionesController.cs
+++ Controllers/RetencionesController.cs
@@ -4,11 +4,13 @@ using Scm.Controllers.Dtos;
 using Scm.Data;
 using Scm.Data.Repositories;
 using CargaDescarga;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Scm.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [Authorize]
     public class RetencionesController:ControllerBase
     {
         RetencionesRepository RetencionesRepository;
@@ -19,14 +21,14 @@ namespace Scm.Controllers
             Context = _context;
             Mapper = _mapper;
         }
-        [HttpPost ("Agregar Retenciones")]
+        [HttpPost ("Agregar-Retenciones")]
         public IActionResult AgregarRetenciones(RetencionesDto Dto){
             Retenciones Retenciones= Mapper.Map<Retenciones>(Dto);
             RetencionesRepository.Insert(Retenciones);
             Context.SaveChanges();
             return Ok(Dto);
         }
-        [HttpPut ("Modificar Retenciones")]
+        [HttpPut ("Modificar-Retenciones")]
         public IActionResult ModificarRetenciones(RetencionesDto Dto){
                 Retenciones Retenciones = Mapper.Map<Retenciones>(Dto);
                 RetencionesRepository.Update(Retenciones);
